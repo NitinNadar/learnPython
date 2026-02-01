89c7fc14c65e374b6253f890f287b5ea114f67e5
Nitin Nadar <nitin.nadar5@gmail.com>
2026-02-01 23:09:25 +0530

    learn python: string slicing, list basics & list operations in python

---
+++ day04.py
@@ -0,0 +1,31 @@
+# string slicing, list basics & list operations in python
+
+# 1. String Slicing in Python
+# string[start : end : step]
+
+text = "PythonProgramming"
+
+# result = text[0:6]
+# result = text[1:6:2]
+
+# 2. List Basics
+numbers = [10,20,30,40,50]
+
+# result = numbers[-1]
+# result = numbers[1:3]
+# result = numbers[::2]
+# result = numbers[0:3:2]
+
+# 3. List Operations
+
+a = [1, 2, 3]
+b = [4, 5]
+
+# result = a + b
+# result = a * 3
+# result = 3 in a
+# result = 3 not in a
+result = len(a)
+
+print(result)
+
