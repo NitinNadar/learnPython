741aca9759fddf604432f2f3f74fe3a55f09c48c
Nitin Nadar <nitin.nadar5@gmail.com>
2026-02-04 23:33:15 +0530

    List Methods, Sorting & Reversing Lists etc

--- day04.py
+++ day04.py
@@ -25,7 +25,66 @@ b = [4, 5]
 # result = a * 3
 # result = 3 in a
 # result = 3 not in a
-result = len(a)
+# result = len(a)
 
-print(result)
+# 4. Important List Methods
 
+nums = [1,2,3]
+nums.append(4) # add value at the end 
+
+nums.insert(1,5) # add the value at specific index (index,value)
+
+nums.extend([6,7,8]) # add multiple value at the end
+
+nums.remove(7) # delete an specific value
+
+nums.pop() # delete the last index value
+
+nums.pop(0) # delete the entered index value
+
+nums.clear() # delete all value
+
+
+# 5. Sorting & Reversing Lists
+
+marks = [45,76,23,18]
+
+# marks.sort() # sort the list and it's origin list
+
+# result = sorted(marks) # sort but without modifing the origin list
+
+marks.reverse()
+
+# 6. Other list methods
+
+set_ofnum = [1,2,3,6,2,7,1,2]
+
+# result = set_ofnum.count(10) # get's the occurrence of the value in the list
+# result = set_ofnum.index(2) # find index of the value - the value of first index present in list
+
+# result = set_ofnum.copy()
+
+# Task 1
+# Reverse a string using slicing.
+
+value = "PythonProgramming"
+
+result = value[::-1]
+
+# Task 2
+# Take a list [5, 10, 15, 20, 25] and print only even numbers using slicing or loop.
+
+num_set = [5, 10, 15, 20, 25]
+
+for num in num_set:
+    if num % 2 == 0:
+        print(num)
+
+for i in range(len(num_set)):
+    if num_set[i] % 2 == 0:
+        print(num_set[i])
+
+# Task 3
+# Add 100 at index 2 in a list.
+
+num_set.insert(2,100)
